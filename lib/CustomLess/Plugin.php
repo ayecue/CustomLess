8a0aa53fd1715b5e1f53352abe6c4d7c7b5a81ee
ayecue <soerenwehmeier@googlemail.com>
2015-04-22 01:21:28 +0200

    for pimcore < v3.0

---
+++ lib/CustomLess/Plugin.php
@@ -0,0 +1,34 @@
+<?php
+
+class CustomLess_Plugin extends CustomLess_Config {
+    const PLUGIN_STACK_INDEX = 1003;
+
+	public static function install() {
+		parent::install();
+
+		return "Plugin successfully installed.";
+    }
+
+    public static function uninstall() {
+		return "Plugin successfully uninstalled.";
+    }
+
+	public static function isInstalled() {
+        return parent::isInstalled();
+	}
+
+	public function preDispatch() {
+
+	 	// Pimcore CDN is not enabled by default in Pimcore.php                  
+		if(!isset($_SERVER['HTTP_SECURE']) && Pimcore_Tool::isFrontend()){
+			//die('Ende');
+			$less = new CustomLess_Controller_Plugin_Parser();
+
+            // 805 means trigger this plugin later than other plugins (with lower numbers)
+			$instance = Zend_Controller_Front::getInstance();
+
+			$instance->registerPlugin($less,self::PLUGIN_STACK_INDEX);
+		}
+	}
+}
+
